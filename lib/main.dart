855d8ff676dca4f6788edf3118a7be859d99e22b
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2023-06-23 11:01:27 +0530

    commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // ignore: unused_field
   int _counter = 0;
   void _incrementCounter() {
     setState(() {
--- lib/pages/traindetails.dart
+++ lib/pages/traindetails.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrainDetails extends StatelessWidget {
   const TrainDetails({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    LatLng staticLocation = LatLng(37.7749, -122.4194); // Static location coordinates (San Francisco, CA)
+
     return Padding(
       padding: const EdgeInsets.all(24.0),
-      child: Column(
-        children: const[
-          Text('Train Details Page', style: TextStyle(fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold),),
-          SizedBox(height: 100,),
-          CircleAvatar(
-            radius: 70,
-            child: Icon(Icons.directions_train, size: 120,),
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: staticLocation,
+          zoom: 15,
+        ),
+        markers: Set<Marker>.from([
+          Marker(
+            markerId: MarkerId('currentLocation'),
+            position: staticLocation,
+            icon: BitmapDescriptor.defaultMarker,
           ),
-          SizedBox(height: 100,),
-          Text('Train Details Page Content', style: TextStyle(fontSize: 30, color: Colors.white),),
-        ],
+        ]),
       ),
     );
   }
